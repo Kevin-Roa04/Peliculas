e7dd2cc92febf99cc1c0a83c8fa11c4dcb1931b7
Kevin-Roa04 <kevin.roa49u@std.uni.edu.ni>
2023-02-01 07:49:19 -0600

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas/src/pages/home_page.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Peliculas',
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomePage(),
+      },
+    );
+  }
+}
---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas/src/widgets/card_swiper_widget.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text('Peliculas en cine'),
+        backgroundColor: Colors.indigoAccent,
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.search))],
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            _swiperTarjetas(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _swiperTarjetas() {
+    return CardSwiper(peliculas: [1, 2, 3, 4, 5]);
+  }
+}
